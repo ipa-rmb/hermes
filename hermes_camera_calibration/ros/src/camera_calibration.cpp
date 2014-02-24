f8638c13d064677a9011230a4b00c4ed46da1d06
ipa-rmb <Richard.Bormann@ipa.fraunhofer.de>
2014-02-24 17:47:46 +0100

    upgrade camera calibration with averaging function for multiple markers

--- hermes_camera_calibration/ros/src/camera_calibration.cpp
+++ hermes_camera_calibration/ros/src/camera_calibration.cpp
@@ -21,46 +21,68 @@ void CameraCalibration::inputCallback(const cob_object_detection_msgs::Detection
 {
 	//std::cout << "Receiving detection of coordinate system..." << std::endl;
 
-	// search for marker No. 1
+	// search for markers
+	int marker_count = 0;
+	tf::Point avg_translation;
+	tf::Quaternion avg_orientation;
 	for (unsigned int i=0; i<input_marker_detections_msg->detections.size(); i++)
 	{
-		if (input_marker_detections_msg->detections[i].label.compare("tag_3") == 0)
+		if (input_marker_detections_msg->detections[i].label.compare("tag_1") == 0 || input_marker_detections_msg->detections[i].label.compare("tag_3") == 0)
 		{
 			// update average coordinate system of marker
 			tf::Point new_translation;
 			tf::pointMsgToTF(input_marker_detections_msg->detections[i].pose.pose.position, new_translation);
 			tf::Quaternion new_orientation;
 			tf::quaternionMsgToTF(input_marker_detections_msg->detections[i].pose.pose.orientation, new_orientation);
-			if (base_translation_.isZero())
+			if (marker_count==0)
 			{
-				// use value directly on first message
-				base_translation_ = new_translation;
-				base_orientation_ = new_orientation;
+				avg_translation = new_translation;
+				avg_orientation = new_orientation;
 			}
 			else
 			{
-				// update value
-				base_translation_ = (1.0-update_rate_) * base_translation_ + update_rate_ * new_translation;
-				base_orientation_.setW((1.0-update_rate_) * base_orientation_.getW() + update_rate_ * new_orientation.getW());
-				base_orientation_.setX((1.0-update_rate_) * base_orientation_.getX() + update_rate_ * new_orientation.getX());
-				base_orientation_.setY((1.0-update_rate_) * base_orientation_.getY() + update_rate_ * new_orientation.getY());
-				base_orientation_.setZ((1.0-update_rate_) * base_orientation_.getZ() + update_rate_ * new_orientation.getZ());
+				avg_translation += new_translation;
+				avg_orientation += new_orientation;
 			}
-
-			// publish new coordinate transformation
-			tf::StampedTransform transform_camera_optframe_to_link;
-			try
-			{
-				transform_listener_.lookupTransform(input_marker_detections_msg->header.frame_id, "/cam3d_link", input_marker_detections_msg->header.stamp, transform_camera_optframe_to_link);
-			}
-			catch (tf::TransformException ex)
-			{
-				ROS_ERROR("%s",ex.what());
-				return;
-			}
-			tf::Transform marker_transform(base_orientation_, base_translation_);
-			transform_broadcaster_.sendTransform(tf::StampedTransform(marker_transform.inverse()*transform_camera_optframe_to_link, input_marker_detections_msg->header.stamp, parent_frame_for_camera_, "/cam3d_link"));
+			++marker_count;
 		}
+	}
+
+	// update averaged coordinate system
+	if (marker_count > 0)
+	{
+		avg_translation /= (double)marker_count;
+		avg_orientation /= (double)marker_count;
+		avg_orientation.normalize();
+		if (base_translation_.isZero())
+		{
+			// use value directly on first message
+			base_translation_ = avg_translation;
+			base_orientation_ = avg_orientation;
+		}
+		else
+		{
+			// update value
+			base_translation_ = (1.0-update_rate_) * base_translation_ + update_rate_ * avg_translation;
+			base_orientation_.setW((1.0-update_rate_) * base_orientation_.getW() + update_rate_ * avg_orientation.getW());
+			base_orientation_.setX((1.0-update_rate_) * base_orientation_.getX() + update_rate_ * avg_orientation.getX());
+			base_orientation_.setY((1.0-update_rate_) * base_orientation_.getY() + update_rate_ * avg_orientation.getY());
+			base_orientation_.setZ((1.0-update_rate_) * base_orientation_.getZ() + update_rate_ * avg_orientation.getZ());
+		}
+
+		// publish new coordinate transformation
+		tf::StampedTransform transform_camera_optframe_to_link;
+		try
+		{
+			transform_listener_.lookupTransform(input_marker_detections_msg->header.frame_id, "/cam3d_link", input_marker_detections_msg->header.stamp, transform_camera_optframe_to_link);
+		}
+		catch (tf::TransformException ex)
+		{
+			ROS_ERROR("%s",ex.what());
+			return;
+		}
+		tf::Transform marker_transform(base_orientation_, base_translation_);
+		transform_broadcaster_.sendTransform(tf::StampedTransform(marker_transform.inverse()*transform_camera_optframe_to_link, input_marker_detections_msg->header.stamp, parent_frame_for_camera_, "/cam3d_link"));
 	}
 }
 
